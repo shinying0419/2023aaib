c40d7177ffac78036e2fcc156d0fccf11b067677
shinying0419 <lishinying05@gmail.com>
2023-12-22 10:52:57 +0800

    week15

---
+++ week15/week15-2b.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        N = len(s) # 長度
+        zero = 0
+        one = 0
+        for i in range(N):
+            if s[i]=='1': one += 1
+        #print('一開始的時候,都在右邊,統計結果', 'zero', zero, 'one', one)
+        ans = 0
+        for i in range(N-1):
+            if s[i]=='0':
+                zero += 1
+            if s[i]=='1':
+                one -= 1
+        #print('中間過程中,','zero', zero, 'one', one)
+            ans = max( ans, zero+one)
+        return ans
